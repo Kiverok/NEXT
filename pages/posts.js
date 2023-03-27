1782038e46605ebb0ae0a58969388d7e3d6a1910
Kiverok <kiver566@ukr.net>
2023-03-27 19:31:50 +0300

    pages

---
+++ pages/posts.js
@@ -0,0 +1,9 @@
+const Posts = () => {
+    return <>
+    <h1>Posts list:</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    </p>
+    </>
+  }
+  
+  export default Posts;
