7c33d8721cd8f1bdb0ac7c1b154e163d8f49049a
Kiverok <kiver566@ukr.net>
2023-04-01 16:46:06 +0300

    fetching

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,7 +1,38 @@
+import Head from "next/head";
+import Link from "next/link";
 import Heading from "../../components/Heading";
 
-const Contacts = () => {
-    return <Heading text="Contacts list:" />
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
   }
-  
-  export default Contacts;
+
+  return {
+    props: { contacts: data },
+  }
+};
+
+const Contacts = ({ contacts }) => { 
+  return (
+    <>
+      <Head>
+        <title>Contacts</title>
+      </Head>
+      <Heading text="Contacts list:" />
+      <ul>
+        {contacts && contacts.map(({ id, name, email }) => (
+          <li key={id}>
+            <Link href={`/contacts/${id}`}>{name} ({email})</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Contacts;
